70759458e090248516c30e276e02aa954ecebb92
YaleCheung <zybjtu@gmail.com>
2017-01-06 11:03:29 +0800

    add not interesting

---
+++ algorithm/countNumberWithSameFirstLast.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+auto digits_num(int n) -> int {
+    if (0 == n)
+    int ret = 1;
+    while(n >> 1) 
+        ++ ret;
+    
+}
+auto solution(int start, int end) -> int {
+    
+}
